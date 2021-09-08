0b11bdec7c11f5a113c1e44df26c36364e3f1086
guinacosta <agnaldo.almeida@iteris.com.br>
2021-09-07 21:53:51 -0300

    simple auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,9 @@
 
 # Articles Controller
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: 'dhh', password: 'secret',
+                               except: %i[index show]
+
   def index
     @articles = Article.all
   end
